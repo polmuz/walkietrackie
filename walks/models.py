d85b138084b9e32c637695860553ffcddd71f531
Pablo Mouzo <pablomouzo@gmail.com>
2014-12-21 22:38:59 -0300

    Initial walks model

---
+++ walks/models.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Walk(models.Model):
+    """
+    A Walk belongs to a user, it has a distance in meters,
+    a duration (time) in seconds and a date.
+    """
+    user = models.ForeignKey(User)
+    distance = models.FloatField()
+    time = models.IntegerField()
+    date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
