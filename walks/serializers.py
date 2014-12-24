185d4b39b09cfb3ef7fe85fd4415006194302907
Pablo Mouzo <pablomouzo@gmail.com>
2014-12-24 15:58:10 -0300

    Improve date display and input format, table data spacing and input hints

--- walks/serializers.py
+++ walks/serializers.py
@@ -4,6 +4,13 @@ from .models import Walk
 
 
 class WalkSerializer(serializers.HyperlinkedModelSerializer):
+    date = serializers.DateTimeField(
+        format='%Y-%m-%d',
+        input_formats=[
+            '%Y-%m-%d',
+        ]
+    )
+
     class Meta:
         model = Walk
         fields = ('distance', 'time', 'date', 'pk')
