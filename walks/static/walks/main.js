0637ff0efb79e358aae598b692dbdef9f1f500b3
Pablo Mouzo <pablomouzo@gmail.com>
2014-12-24 13:03:16 -0300

    Add weekly average stats

--- walks/static/walks/main.js
+++ walks/static/walks/main.js
@@ -13,13 +13,34 @@ myApp.controller('WalksController', ['$scope', '$resource', function($scope, $re
     }
   );
 
-  $scope.walks = Walk.query();
+
+
+  $scope.refreshWalks = function() {
+    var walks = Walk.query(function(){
+      $scope.weekStats = {};
+      for (var i = 0; i < walks.length; i++) {
+        var walk = walks[i];
+        var date = new Date(walk.date);
+        var week = date.format("Y-\\WW");
+        var stats = $scope.weekStats[week] || {distance:0, time:0, walks:0};
+        $scope.weekStats[week] = {
+          distance: stats.distance + walk.distance,
+          time: stats.time + walk.time,
+          walks: stats.walks + 1
+        }
+      }
+
+      return true;
+    });
+
+    $scope.walks = walks;
+    return true;
+  };
+
+  $scope.refreshWalks();
   $scope.newWalk = {};
   $scope.dateFilter = {};
 
-  $scope.refreshWalks = function() {
-    $scope.walks = Walk.query();
-  };
 
   $scope.saveNewWalk = function() {
     var walk = new Walk($scope.newWalk);
@@ -30,8 +51,10 @@ myApp.controller('WalksController', ['$scope', '$resource', function($scope, $re
   }
 
   $scope.removeWalk = function(walk) {
-    walk.$remove();
-    $scope.refreshWalks();
+    walk.$remove(function(){
+      $scope.refreshWalks();
+    });
+
   };
 
   $scope.clearDateFilter = function() {
